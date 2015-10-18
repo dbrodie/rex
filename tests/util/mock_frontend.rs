4fc8c95f68ed08d463b79422e0d1d5c6801b037f
Daniel Brodie <daniel@brodienet.com>
2015-10-18 23:07:40 +0300

    Create some basic testing using a mock frontend

---
+++ tests/util/mock_frontend.rs
@@ -0,0 +1,78 @@
+use rex::frontend::{Frontend, Event, Style, KeyPress};
+use rex::ui::view::HexEdit;
+
+pub struct MockFrontend {
+    cursor: (usize, usize),
+    size: (usize, usize),
+}
+
+impl MockFrontend {
+    pub fn new() -> MockFrontend {
+        MockFrontend {
+            cursor: (0, 0),
+            size: (100, 100),
+        }
+    }
+
+    pub fn run_str(&mut self, edit: &mut HexEdit, s: &str) {
+        for c in s.chars() {
+            edit.input(KeyPress::Key(c));
+            edit.draw(self);
+        }
+    }
+
+    pub fn run_keys<I>(&mut self, edit: &mut HexEdit, keys: I) where
+            I: IntoIterator<Item=KeyPress> {
+        for key in keys {
+            edit.input(key);
+            edit.draw(self);
+        }
+    }
+
+    pub fn run_events<I>(&mut self, edit: &mut HexEdit, events: I) where
+            I: IntoIterator<Item=Event> {
+        for event in events {
+            match event {
+                Event::KeyPressEvent(key) => edit.input(key),
+                Event::Resize(w, h) => {
+                    self.size = (w, h);
+                    edit.resize(w as i32, h as i32)
+                }
+            }
+            edit.draw(self);
+        }
+    }
+}
+
+impl Frontend for MockFrontend {
+    fn clear(&self) {
+    }
+
+    fn present(&self) {
+    }
+
+    fn print_style(&self, _x: usize, _y: usize, _style: Style, _s: &str) {
+    }
+
+    fn print_char_style(&self, _x: usize, _y: usize, _style: Style, _c: char) {
+    }
+
+    fn print_slice_style(&self, _x: usize, _y: usize, _style: Style, _chars: &[char]) {
+    }
+
+    fn set_cursor(&mut self, x: isize, y: isize) {
+        self.cursor = (x as usize, y as usize);
+    }
+
+    fn height(&self) -> usize {
+        self.size.1
+    }
+
+    fn width(&self) -> usize {
+        self.size.0
+    }
+
+    fn poll_event(&mut self) -> Event {
+        panic!("Unimplemented!");
+    }
+}
---
+++ tests/util/mod.rs
@@ -0,0 +1,21 @@
+pub mod mock_frontend;
+
+use rex::config::Config;
+use rex::ui::view::HexEdit;
+
+fn generate_vec(size: usize) -> Vec<u8> {
+    (0..size).map(|x| (x & 0xff) as u8).collect()
+}
+
+pub fn simple_init(size: usize) -> (HexEdit, mock_frontend::MockFrontend) {
+    let config : Config = Default::default();
+
+    let mut edit = HexEdit::new(config);
+    let mut frontend = mock_frontend::MockFrontend::new();
+
+    edit.open_vec(generate_vec(size));
+
+    edit.resize(100, 100);
+    edit.draw(&mut frontend);
+    (edit, frontend)
+}
